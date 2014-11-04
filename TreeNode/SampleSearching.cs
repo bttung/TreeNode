c3e89cb726a50edfc863c6c08a4ffd04ea725e99
Bui Thanh Tung <tungfcb@gmail.com>
2014-11-05 08:05:53 +0900

    Added yet-another-tree-structure project file!
    https://code.google.com/p/yet-another-tree-structure/source/browse/trunk/csharp/CSharpTree/

---
+++ TreeNode/SampleSearching.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class SampleSearching
+{
+	static void Main(string[] args)
+	{
+		TreeNode<string> treeRoot = SampleData.GetSet1();
+		TreeNode<string> found = treeRoot.FindTreeNode(node => node.Data != null && node.Data.Contains("210"));
+
+		Console.WriteLine("Found: " + found);
+	}
+}
